324636dc1b32290179d3ea56df333f00af2588f7
jeromeformation-restitution <jerome@gmail.com>
2019-08-09 09:34:22 +0200

    Tchat: envoi et reception de messages

--- public/js/tchat-client.js
+++ public/js/tchat-client.js
@@ -1,21 +1,40 @@
 console.log('test');
 
 let socket = io();
-
+var username;
 $('#tchatConnection').on('submit', tchatConnection);
-
 $('#tchat').hide();
 function tchatConnection(event) {
     event.preventDefault();
-    var usernamme = $('#username').val();
-
-    socket.emit("tchatConnection", {usernamme: usernamme});
+    username = $('#username').val();
+    socket.emit("tchatConnection", {username: username});
     socket.on('initTchat', initTchat);
-    
 }
-
+function sendMessage(){
+    let message = $('.write_msg').val();
+    socket.emit('nouveau-message', {
+        username:username,
+        message:message
+    });
+}
 function initTchat () {
     $('#tchatConnection').fadeOut();
     $('#tchat').fadeIn();
+    $('.msg_send_btn').on('click', sendMessage);
+    socket.on('broadcast', newMsg);
+}
+function newMsg(datas){
 
+    let html = `
+            <div class="incoming_msg">
+              <div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div>
+              <div class="received_msg">
+                <div class="received_withd_msg">
+                    <span class="time_date"> ${datas.username}</span>
+                  <p>${datas.message}</p>
+                 </div>
+              </div>
+            </div>
+    `;
+    $('.msg_history').append(html);
 }
--- web-socket-io/tchat-server.js
+++ web-socket-io/tchat-server.js
@@ -1,22 +1,22 @@
+let allSockets;
 module.exports = (io) => {
     io.on('connection', connection)
+    allSockets = io;
 };
-
 function connection(socket) {
     console.log('Je suis connecté en tant que client');
-
     socket.on('tchatConnection', (data)=> {
         tchatConnection(data, socket)
     });
+    socket.on('nouveau-message', newMsg);
 }
-
-
 function tchatConnection(data, socket) {
     tableauUtilisateur [socket.id] = data.usernamme;
     console.log(tableauUtilisateur);
-
     socket.emit('initTchat', {message: []});
 }
-
+function newMsg(datas) {
+    allSockets.emit('broadcast', datas);
+}
 const tableauUtilisateur = [];
 
